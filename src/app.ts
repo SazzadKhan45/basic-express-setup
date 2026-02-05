f03ed1b9e811d6d3edd7ce3041905bcc31cbb1af
Sazzadkhan45 <sazzadkhan753@gmail.com>
2026-02-05 12:39:40 +0600

    Basic express server all setUp done

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
 import express, { Request, Response } from "express";
 const app = express();
 
+// Testing get api
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.status(201).json({
+    success: true,
+    message: `Express Server is running on port : ${process.env.PORT}`,
+  });
+});
+
+// Not Fount Api
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: "Routes Not Found !",
+    path: req.path,
+  });
 });
 
 // default export
